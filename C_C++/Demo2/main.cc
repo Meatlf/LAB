7790e277ddb4ebfafa4563ed9af1c6aae565db66
Meatlf <454779758@qq.com>
2019-03-17 22:29:08 -0700

    Add Linux version

--- C_C++/Demo2/main.cc
+++ C_C++/Demo2/main.cc
@@ -1,8 +1,9 @@
 #include <stdio.h>
+//#define linux
 
 int main(void)
 {
-	char buffer[200], s[] = "B", c = 'l';
+	char buffer[200], s[] = "HelloWorld!", c = 'l';
 	int i = 35, j;
 	float fp = 1.7320534f;
 	// Format and print various data:
@@ -11,13 +12,14 @@ int main(void)
 	j += sprintf_s(buffer + j, sizeof(buffer)- j, " C%c\n", c);
 	j += sprintf_s(buffer + j, sizeof(buffer)- j, " Integer: %d\n", i);
 	j += sprintf_s(buffer + j, sizeof(buffer)- j, " Real: %f\n", fp);
+	printf_s("Output:\n%s\ncharacter count = %d\n", buffer, j);
 #endif
 
 #ifdef linux
-	snprintf(buffer, sizeof(buffer), "%s\n", s); 
-    	printf("%sn",buffer);
+	j=snprintf(buffer, sizeof(buffer), "%s\n", s); 
+    	printf("%sn\n",buffer);
+	printf("%d\n",j);
 #endif
-	printf_s("Output:\n%s\ncharacter count = %d\n", buffer, j);
 	printf("Done!\n");
 	return 0;
 }
