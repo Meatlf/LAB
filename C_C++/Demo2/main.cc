07c4dffd94b988bf78d7fbbae8a8d1e8f9bca556
Meatlf <454779758@qq.com>
2019-03-18 11:03:48 +0800

    temp

--- C_C++/Demo2/main.cc
+++ C_C++/Demo2/main.cc
@@ -1,14 +1,22 @@
 #include <stdio.h>
+
 int main(void)
 {
 	char buffer[200], s[] = "B", c = 'l';
 	int i = 35, j;
 	float fp = 1.7320534f;
 	// Format and print various data:
-	j = sprintf_s(buffer, 200, "A%s\n", s);
-	j += sprintf_s(buffer + j, 200 - j, " C%c\n", c);
-	j += sprintf_s(buffer + j, 200 - j, " Integer: %d\n", i);
-	j += sprintf_s(buffer + j, 200 - j, " Real: %f\n", fp);
+#ifdef _WIN32
+	j = sprintf_s(buffer, sizeof(buffer), "A%s\n", s);
+	j += sprintf_s(buffer + j, sizeof(buffer)- j, " C%c\n", c);
+	j += sprintf_s(buffer + j, sizeof(buffer)- j, " Integer: %d\n", i);
+	j += sprintf_s(buffer + j, sizeof(buffer)- j, " Real: %f\n", fp);
+#endif
+
+#ifdef linux
+	snprintf(buffer, sizeof(buffer), "%s\n", s); 
+    	printf("%sn",buffer);
+#endif
 	printf_s("Output:\n%s\ncharacter count = %d\n", buffer, j);
 	printf("Done!\n");
 	return 0;
