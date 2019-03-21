666b72ca2af09a2bb8f2b8f2f56fefc662316da0
ttz <454779758@qq.com>
2019-03-20 22:53:41 -0700

    Modified

--- C_C++/Demo1/main.cc
+++ C_C++/Demo1/main.cc
@@ -1,8 +1,8 @@
 #include<stdio.h>
 
 int main(int argc, char * argv[]){
-    printf("%s",argv[0]);
-    printf("%s",argv[1]);
-	printf("%d", 5);
+    printf("%s\n",argv[0]);
+    printf("%s\n",argv[1]);
+    printf("%d\n", 5);
 	return 0;
 }
