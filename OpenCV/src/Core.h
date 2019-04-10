3583a759971e2324a77709572bdcf8b61699df56
Meatlf <454779758@qq.com>
2019-04-10 15:50:43 +0800

    Add

---
+++ OpenCV/src/Core.h
@@ -0,0 +1,8 @@
+#ifndef _CORE_H
+#define _CORE_H
+
+namespace Core {
+	void PictureCopy();
+}
+
+#endif
---
+++ OpenCV/src/define.h
@@ -0,0 +1,7 @@
+#ifndef _DEFINE_H
+#define _DEFINE_H
+
+#define WIDTH 400
+#define HEIGHT 400
+
+#endif
