49143e3c5852bebc7ae038f6292f03488a4ec2ad
TTZ <454779758@qq.com>
2019-04-13 16:48:59 +0800

    Add code about C++ Constructors and destructors

--- C++/ConstructorsAndDestructors/main.cpp
+++ C++/ConstructorsAndDestructors/main.cpp
@@ -1,14 +1,33 @@
+/*
+功能：
+	C++类构造函数和析构函数的基本使用实例
+说明：
+     知识点包括构造函数和析构函数的声明、定义和初始化、使用初始化列表来初始化字段。
+函数：
+
+作者：tztang
+
+参考资料：
+[C++ 类构造函数 & 析构函数](http://www.runoob.com/cplusplus/cpp-constructor-destructor.html)
+
+日期：
+2019-04-13
+*/
+
 #include <iostream>
 
 using namespace std;
+
+#define UseInitializationList  (1)	// 是否使用初始化列表
 
 class Line
 {
 public:
 	void setLength(double len);
 	double getLength(void);
-	Line();  // 这是构造函数
-
+	Line();  // 这是构造函数声明
+	Line(double len);
+	~Line();
 private:
 	double length;
 };
@@ -17,6 +36,18 @@ private:
 Line::Line(void)
 {
 	cout << "Object is being created" << endl;
+}
+
+#if UseInitializationList 
+Line::Line(double len) :length(len) {}
+#else
+Line::Line(double len) {
+	length = len;
+}
+#endif
+
+Line::~Line(void) {
+	cout << "Object is being deleted" << endl;
 }
 
 void Line::setLength(double len)
@@ -28,14 +59,20 @@ double Line::getLength(void)
 {
 	return length;
 }
-// 程序的主函数
+
 int main()
 {
-	Line line;
+	Line line1;
+	Line line2(10.0);
 
 	// 设置长度
-	line.setLength(6.0);
-	cout << "Length of line : " << line.getLength() << endl;
+	line1.setLength(6.0);
+	cout << "Length of line1 : " << line1.getLength() << endl;
+
+	cout << "Length of line2 : " << line1.getLength() << endl;
+
+	line2.setLength(12.0);
+	cout << "Length of line2 : " << line2.getLength() << endl;
 
 	return 0;
 }
