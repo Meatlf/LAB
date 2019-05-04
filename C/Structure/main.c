f1ce2db4082376b0903503e6dfd3e8c05789f6fc
TTZ <454779758@qq.com>
2019-05-04 22:59:25 +0800

    Modified

--- C/Structure/main.c
+++ C/Structure/main.c
@@ -17,10 +17,15 @@ int main()
 {
 	printf("A1's address is %x\n", &A1);
 	printf("A1.a's address is %x\n", &A1.a);
-	printf("A1.a's size is %d\n",(unsigned int)(void*)&A1.b - (unsigned int)(void*)&A1.a);
+	printf("A1.a's size is %d\n", (unsigned int)(void*)&A1.b - (unsigned int)(void*)&A1.a);
 	printf("A1.b's size is %d\n", (unsigned int)(void*)&A1.c - (unsigned int)(void*)&A1.b);
 	printf("A1's size is %d\n", sizeof(A1));
-	//printf("A1.c size is %d\n", sizeof(A1) - (unsigned int)(void*)&A1.b);
+
+	printf("B1's Bddress is %x\n", &B1);
+	printf("B1.b's Bddress is %x\n", &B1.b);
+	printf("B1.a's size is %d\n", (unsigned int)(void*)&B1.a - (unsigned int)(void*)&B1.b);
+	printf("B1.b's size is %d\n", (unsigned int)(void*)&B1.c - (unsigned int)(void*)&B1.a);
 	printf("B1's size is %d\n", sizeof(B1));
+
 	return 0;
 }
