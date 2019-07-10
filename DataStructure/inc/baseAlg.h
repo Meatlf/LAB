3c4a1fe760cbe03c62fba7e866cd121251901db6
TTZ <454779758@qq.com>
2019-07-10 21:53:05 +0800

    Added 17

--- DataStructure/inc/baseAlg.h
+++ DataStructure/inc/baseAlg.h
@@ -31,5 +31,34 @@ namespace BASEALG
 
 	// 面试题17：打印从1到最大的n位数
 	// 题目：输入数字n，按顺序打印出从1到最大的n位十进制数。比如输入3，则打印出1、2、3一直到最大的3位数999。
+	/*
+	解题：
+	*/
+	/*
+	方法1：暴力法
+	评价：会造成溢出，健壮性差的代码
+	*/
 	void print1ToMaxOfNDigits(int n);
+
+	/*
+	方法2：用字符串来解决大数问题
+	解法：
+		首先把字符串中的每一个数字都初始化为'0'，然后每一次为字符串表示的数字加1，再打印出来。
+		要做两件事：一是在字符串表达的数字上模拟加法；二是把字符串表达的数字打印出来。
+	*/
+	void printToMaxOfNDigitsOpt1(int n);
+	/*
+	功能：函数increment()实现在表示的字符串number上增加1
+	输入：用字符串表示的整数
+	输出：溢出标志
+	知识点：字符运算
+	*/
+	bool increment(char* number);
+	void printNumber(char* number);
+
+	/*
+	方法3：递归法
+	*/
+	void printToMaxOfNDigitsOpt2(int n);
+	void print1ToMaxOfNDigitsRecursively(char* number, int length, int index);
 }
--- DataStructure/src/baseAlg.cc
+++ DataStructure/src/baseAlg.cc
@@ -13,8 +13,7 @@ bool bInvalidInput = false;
 double BASEALG::powerOpt1(double base, int exponent)
 {
 	bInvalidInput = false;
-	double result;
-	
+
 	if (base == 0.0 && exponent < 0)
 	{
 		bInvalidInput = true;
@@ -34,7 +33,7 @@ double BASEALG::powerOpt1(double base, int exponent)
 
 double BASEALG::powerOpt2(double base, int exponent)
 {
-	
+
 	return 0.0;
 }
 
@@ -62,4 +61,113 @@ void BASEALG::print1ToMaxOfNDigits(int n)
 
 	for (i = 1;i < number;++i)
 		std::cout << i;
+}
+
+void BASEALG::printToMaxOfNDigitsOpt1(int n)
+{
+	if (n <= 0)
+		return;
+
+	char* number = new char[n + 1];
+	memset(number, '0', n);
+	number[n] = '\0';
+
+	while (!increment(number))
+	{
+		printNumber(number);
+	}
+	delete[]number;
+}
+
+/*
+数组与整数的关系：
+下标：[0][1][2]
+ 值：  9  8 \n
+高位、低位，
+表示的整数为98.
+*/
+bool BASEALG::increment(char * number)
+{
+	bool isOverflow = false;				// 溢出标志位
+	int nTakeOver = 0;						// 进位
+	int nLength = strlen(number);			// 字符串的长度（不包含'\0'）
+
+	// 从低位到高位处理字符串
+	for (int i = nLength - 1;i >= 0;i--)
+	{
+		int nSum = number[i] - '0' + nTakeOver;
+
+		// 最低位
+		if (i == nLength - 1)
+			nSum++;
+
+		// 进位
+		if (nSum >= 10)
+		{
+			// 最高位的进位表示溢出
+			if (i == 0)
+				isOverflow = true;
+			else
+			{
+				nSum -= 10;
+				nTakeOver = 1;
+				number[i] = '0' + nSum;
+			}
+		}
+		else
+		{
+			number[i] = '0' + nSum;
+			break;
+		}
+	}
+	return isOverflow;
+}
+
+void BASEALG::printNumber(char * number)
+{
+	bool isBeginning0 = true;
+	int nLength = strlen(number);
+
+	for (int i = 0;i < nLength;++i)
+	{
+		if (isBeginning0 && number[i] != '0')
+			isBeginning0 = false;
+
+		if (!isBeginning0)
+		{
+			printf("%c", number[i]);
+		}
+	}
+	printf("\t");
+}
+
+void BASEALG::printToMaxOfNDigitsOpt2(int n)
+{
+	if (n <= 0)
+		return;
+
+	char* number = new char[n + 1];
+	number[n] = '\0';
+
+	for (int i = 0;i < 10;++i)
+	{
+		number[0] = i + '0';
+		print1ToMaxOfNDigitsRecursively(number, n, 0);
+	}
+	delete[]number;
+}
+
+void BASEALG::print1ToMaxOfNDigitsRecursively(char * number, int length, int index)
+{
+	if (index == length - 1)
+	{
+		printNumber(number);
+		return;
+	}
+
+	for (int i = 0;i < 10;++i)
+	{
+		number[index + 1] = i + '0';
+		print1ToMaxOfNDigitsRecursively(number, length, index + 1);
+	}
 }
