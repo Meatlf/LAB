71ab80255cdf00de1707ce0437d174f88c0eabb0
Meatlf <454779758@qq.com>
2019-12-08 04:00:04 -0500

    20191208

---
+++ Python/Python编程：从入门到实践/Chapter3-列表简介/3-1-姓名.py
@@ -0,0 +1,4 @@
+names = ['Zhao', 'Qian', 'Sun', 'Li']
+print(names[0])
+print(names[1])
+print(names[2])
---
+++ Python/Python编程：从入门到实践/Chapter3-列表简介/3-2-问候语.py
@@ -0,0 +1,4 @@
+names = ['Zhao', 'Qian', 'Sun', 'Li']
+print("My good friend's name is " + names[0] + ".") 
+print("My good friend's name is " + names[1] + ".") 
+print("My good friend's name is " + names[2] + ".") 
