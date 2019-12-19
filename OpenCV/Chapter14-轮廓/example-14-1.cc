2dcbf67c2cc32589ca4b0a831351a3405c6a382b
Meatlf <454779758@qq.com>
2019-12-19 08:31:04 -0500

    20191219

--- OpenCV/Chapter14-轮廓/example-14-1.cc
+++ OpenCV/Chapter14-轮廓/example-14-1.cc
@@ -5,3 +5,11 @@ cv::Mat g_gray, g_binary;
 
 int g_thresh = 100;
 
+void on_trackbar(int, void*){
+    cv::threshold(g_gray,g_binary,g_thresh,255,cv::THRESH_BINARY);
+    std::vector<std::vector<cv::Point>> contours;
+    cv::findContours(g_binary,contours,cv::noArray(),cv::RETR_LIST,cv::CHAIN_APPROX_SIMPLE);
+    g_binary = cv::Scalar::all(0);
+
+    cv::draw();
+}
